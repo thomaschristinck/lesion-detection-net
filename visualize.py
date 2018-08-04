a8fdb074e4c039ef1663d5abd1b042d98830a261
Thomas Christinck <thomas.christinck@mail.mcgill.ca>
2018-08-04 15:44:02 -0400

    need to get fns for bboxes

--- visualize.py
+++ visualize.py
@@ -16,6 +16,7 @@ import imageio
 from skimage.measure import find_contours
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 if "DISPLAY" not in os.environ:
     plt.switch_backend('agg')
 import matplotlib.patches as patches
@@ -321,21 +322,24 @@ def get_mask_color(mask, gt_masks, gt_labels, gt_nles):
     """
     # Number of boxes
     max_intersect = 0
+    gt_idx = 0
     for i in range(1, gt_nles + 1):
         # Go through each lesion and see if max intersect is > 0.5 * lesion size, or > 3 voxels
         # if it is, the instance mask is green (TP), otherwise, it is blue (undetected FN).
         lesion_size = np.sum(gt_masks[gt_labels == i])
-        intersect = mask[gt_labels == 1].sum()
+        intersect = mask[gt_labels == i].sum()
         if intersect >= 3 or intersect >= 0.5 * lesion_size:
             max_intersect = intersect
+            gt_idx = i
 
     N=2
+    print('Max intersect : ', max_intersect)
     if max_intersect != 0:
         # Color mask green; it is a true positive
-        hsv = [(0.42, 0.67, 1.0) for i in range(N)]
+        hsv = [(0.38, 0.88, 0.8) for i in range(N)]
         colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
         color = colors[0]
-        return color, i
+        return color, gt_idx
 
     else:
         # Color mask red; it is a false positive
@@ -349,9 +353,7 @@ def get_box_color(box, masks):
     box_matrix = np.zeros((masks.shape[0], masks.shape[1]))
     box_matrix[y1:y2, x1:x2,] = 1
     box_size = np.sum(box_matrix)
-    print('box size, mask sum :', box_size, np.sum(masks))
     intersect = np.sum(box_matrix * masks)
-    print('intersect: ', intersect )
 
     # Now for the union, figure out which lesion is contributing to intersect, then 
     # union is the sum of the lesion size plus box size - minus the intersect
@@ -373,9 +375,7 @@ def get_box_color(box, masks):
        
     
     # Then IoU:
-    print('Intersect: ', intersect)
     union = box_size + max_size - intersect
-    print('Union: ', union)
     iou = intersect / union
 
     # Reset mask
@@ -384,14 +384,23 @@ def get_box_color(box, masks):
 
     labels, nles2 = ndimage.label(masks)
     if intersect != 0:
-        print('ya')
         # True positive
-        color = 'g'
+        color = 'xkcd:bright green'
     else:
         # False negative
         color = 'r'
     
     return color
+
+def draw_unc_mask(t2, unc):
+    _, ax = plt.subplots(1, figsize=(12, 12))
+
+    # Show area outside image boundaries.
+    ax.axis('off')
+    ax.imshow(t2, cmap=plt.cm.gray, interpolation='none')
+    ax.imshow(unc, cmap=plt.cm.afmhot, alpha=0.7, interpolation='none')
+    return ax
+
 
 def draw_boxes(image, boxes=None, refined_boxes=None,
                mask=None, gt_mask=None, captions=None, visibilities=None,
@@ -478,9 +487,15 @@ def draw_boxes(image, boxes=None, refined_boxes=None,
                     bbox={'facecolor': color, 'alpha': 0.4,
                           'pad': 1, 'edgecolor': 'none'})
 
-    # Masks
+    
+    #------------------------Masks------------------------------
+    # Two options: can display masks as one mask with one color, or can display them as
+    # TPs = green, FPs = red, FNs = blue
+
     if mask is not None and pn_labels == False:
         print('Mask shape: ', mask.shape)
+        colors = random_colors(2, bright=True)
+        color = colors[0]
         masked_image = apply_mask(masked_image, mask, color)
         # Mask Polygon
         # Pad to ensure proper polygons for masks that touch image edges.
@@ -513,24 +528,53 @@ def draw_boxes(image, boxes=None, refined_boxes=None,
                 mask[labels != i] = 0
                 mask[labels == i] = 1
                 masks[i-1] = mask
+            for i in range(1, nles + 1):
+                mask[labels == i] = 1
+
+            print('Masks shape :', masks.shape[0])
             gt_idx_list = []
             for i in range(masks.shape[0]):
-                color, gt_idx = get_mask_color(masks[i], gt_mask, gt_labels, gt_nles)
+                mask_color, gt_idx = get_mask_color(masks[i], gt_mask, gt_labels, gt_nles)
                 gt_idx_list.append(gt_idx)
-                masked_image = apply_mask(masked_image, masks[i], color)
+                print('Masks shape (for i in masks shape[0]):', masks.shape[0])
+                print('Mask color : ', mask_color)
+                masked_image = apply_mask(masked_image, masks[i], mask_color)
                 # Mask Polygon
                 # Pad to ensure proper polygons for masks that touch image edges.
                 padded_mask = np.zeros(
-                    (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-                padded_mask[1:-1, 1:-1] = mask
+                    (masks[i].shape[0] + 2, masks[i].shape[1] + 2), dtype=np.uint8)
+                padded_mask[1:-1, 1:-1] = masks[i]
                 contours = find_contours(padded_mask, 0.5)
                 for verts in contours:
                     # Subtract the padding and flip (y, x) to (x, y)
                     verts = np.fliplr(verts) - 1
-                    p = Polygon(verts, facecolor=color, edgecolor=color)
+                    p = Polygon(verts, facecolor=mask_color, edgecolor=mask_color)
                     ax.add_patch(p)
 
-    ax.imshow(masked_image.astype(np.uint32), cmap=plt.cm.gray_r)
+            # Now go through the gt_idx list and get indices for lesions not detected (FNs):
+            fn_indices = [gt_idx for gt_idx in range(1, gt_nles + 1) if gt_idx not in gt_idx_list]
+            if len(fn_indices) != 0:
+                for i in fn_indices:
+                    gt_mask[gt_labels != i] = 0
+                    gt_mask[gt_labels == i] = 1
+                    masked_image = apply_mask(masked_image, gt_mask, mask_color)
+                    # Mask Polygon
+                    # Pad to ensure proper polygons for masks that touch image edges.
+                    padded_mask = np.zeros(
+                        (gt_mask.shape[0] + 2, gt_mask.shape[1] + 2), dtype=np.uint8)
+                    padded_mask[1:-1, 1:-1] = gt_mask
+                    contours = find_contours(padded_mask, 0.5)
+                    for verts in contours:
+                        # Subtract the padding and flip (y, x) to (x, y)
+                        verts = np.fliplr(verts) - 1
+                        p = Polygon(verts, facecolor='b', edgecolor='b')
+                        ax.add_patch(p)
+                for i in range(1, gt_nles + 1):
+                    gt_mask[gt_labels == i] = 1
+            else:
+                pass
+
+    ax.imshow(masked_image.astype(np.uint32), cmap=plt.cm.gray)
     return ax
 
 def plot_loss(loss, val_loss, save=True, log_dir=None):
@@ -622,18 +666,23 @@ def plot_loss(loss, val_loss, save=True, log_dir=None):
         plt.pause(0.1)
 
 class IndexTracker(object):
-    def __init__(self, ax, X, Y, Z, W):
+    def __init__(self, ax, fig, X, Y, Z, W, U):
         self.ax = ax
+        self.fig = fig
         plt.subplots_adjust(top=0.9)
         self.ax[0][0].axis('off')
         self.ax[0][1].axis('off')
+        self.ax[0][2].axis('off')
         self.ax[1][0].axis('off')
         self.ax[1][1].axis('off')
-        fontdict = {'fontsize':10}
-        self.ax[0][0].set_title(r'Det-Net Output, GT Mask, and T2 Image (confidence thresh = 0.95)', fontdict=fontdict)
-        self.ax[0][1].set_title(r'Ground Truth Lesion Mask', fontdict=fontdict)
-        self.ax[1][0].set_title(r'U-Net Output ($\sigma=0.5$)', fontdict=fontdict)
-        self.ax[1][1].set_title(r'U-Net MC Sample Variance', fontdict=fontdict)
+        self.ax[1][2].axis('off')
+
+        #fontdict = {'fontsize':10}
+        #self.ax[0][0].set_title(r'Det-Net Output, GT Mask, and T2 Image (confidence thresh = 0.95)', fontdict=fontdict)
+        #self.ax[0][1].set_title(r'Ground Truth Lesion Mask', fontdict=fontdict)
+        #self.ax[1][0].set_title(r'U-Net Output ($\sigma=0.5$)', fontdict=fontdict)
+        #self.ax[1][1].set_title(r'U-Net MC Sample Variance', fontdict=fontdict)
+        self.U = U
         self.W = W
         self.X = X
         self.Y = Y
@@ -641,9 +690,10 @@ class IndexTracker(object):
         x_rows, x_cols,  self.slices, x_colors = X.shape
         self.ind = self.slices//2
         self.im1 = ax[0][0].imshow(self.X[:,:, self.ind,:])
-        self.im2 = ax[0][1].imshow(self.Y[:,:, self.ind], cmap=plt.cm.gray_r)
-        self.im3 = ax[1][0].imshow(self.Z[:,:, self.ind], cmap=plt.cm.afmhot_r)
-        self.im4 = ax[1][1].imshow(self.W[:,:, self.ind], cmap=plt.cm.afmhot_r)
+        self.im2 = ax[0][1].imshow(self.Y[:,:, self.ind, :])
+        self.im3 = ax[0][2].imshow(self.Z[:,:, self.ind, :])
+        self.im4 = ax[1][0].imshow(self.W[:,:, self.ind, :])
+        self.im5 = ax[1][1].imshow(self.U[:,:, self.ind], cmap=plt.cm.gray)
         self.update()
 
     def onscroll(self, event):
@@ -659,15 +709,17 @@ class IndexTracker(object):
         self.im2.set_data(self.Y[:,:,self.ind])
         self.im3.set_data(self.Z[:,:,self.ind])
         self.im4.set_data(self.W[:,:,self.ind])
+        self.im5.set_data(self.U[:,:,self.ind])
         self.ax[0][0].set_ylabel('slice %s' % self.ind)
         self.im1.axes.figure.canvas.draw_idle()
         self.im2.axes.figure.canvas.draw_idle()
         self.im3.axes.figure.canvas.draw_idle()
         self.im4.axes.figure.canvas.draw_idle()
+        self.im5.axes.figure.canvas.draw_idle()
 
-def scroll_display(image1, image2, image3, image4, figsize):
-    fig, ax = plt.subplots(2,2, figsize=(20,20))
-    tracker = IndexTracker(ax, image1, image2, image3, image4)
+def scroll_display(image1, image2, image3, image4, image5, figsize):
+    fig, ax = plt.subplots(2,3, figsize=(20,20))
+    tracker = IndexTracker(ax, fig, image1, image2, image3, image4, image5)
     fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
     plt.tight_layout()
     plt.show()
@@ -685,10 +737,12 @@ def build_image3d(t2, target, netseg, unc, threshed, model, class_names, title="
     # Iterate through image slices of depth t2.shape[2]
     depth = t2.shape[2]
     fig, ax = plt.subplots(2,2, figsize=(20,20))
+   
     ax[0][0].axis('off')
     ax[0][1].axis('off')
     ax[1][0].axis('off')
     ax[1][1].axis('off')
+
     for idx in range(depth):
         target_slice = target[:,:,idx]
         threshed_slice = threshed[:,:,idx]
@@ -710,41 +764,78 @@ def build_image3d(t2, target, netseg, unc, threshed, model, class_names, title="
 
         # Number of instances
         N = boxes.shape[0]
+        _, nles_nseg = ndimage.label(threshed_slice)
+        _, nles_gt = ndimage.label(threshed_slice)
 
         # Generate bounding box slices
         if N != 0:
             assert N == masks.shape[-1] == class_ids.shape[0]
-            ax[0][0] = draw_boxes(t2_slice, boxes=boxes, mask=threshed_slice, gt_mask=target_slice, pn_labels=True)
+            ax[0][0] = draw_boxes(t2_slice, boxes=boxes, gt_mask=target_slice, pn_labels=True)
             #ax[0][0] = draw_boxes(t2_slice, boxes=boxes, captions=scores)
         else:
-            ax[0][0] = draw_boxes(t2_slice)       
+            ax[0][0] = draw_boxes(t2_slice) 
 
         plt.savefig(join('/usr/local/data/thomasc/outputs/det_net', str(idx) + '.png'), bbox_inches='tight')
+        plt.close('all')
+
+        if nles_nseg > 0 or nles_gt > 0:
+            ax[0][0] = draw_boxes(t2_slice, gt_mask=target_slice, mask=threshed_slice, pn_labels=True)
+        else:
+            ax[0][0] = draw_boxes(t2_slice)     
+
+        plt.savefig(join('/usr/local/data/thomasc/outputs/det_net', str(idx) + str(idx) + '.png'), bbox_inches='tight')
+        plt.close('all')
+
+        ax[0][0] = draw_boxes(t2_slice, mask=target_slice)  
+
+        plt.savefig(join('/usr/local/data/thomasc/outputs/det_net', 'target' + str(idx) + '.png'), bbox_inches='tight')
+        plt.close('all')
+
+        ax[0][0] = draw_unc_mask(t2_slice, unc_slice)  
+
+        plt.savefig(join('/usr/local/data/thomasc/outputs/det_net', 'unc' + str(idx) + '.png'), bbox_inches='tight')
         plt.close('all')
 
     print('---------- Reading images into numpy array ------------')
 
     im = imageio.imread(join('/usr/local/data/thomasc/outputs/det_net', str(idx) + '.png'), as_gray=False, pilmode="RGB")
     boxed_image = np.zeros([depth, im.shape[0], im.shape[1], im.shape[2]])
+    nseg_image = np.zeros([depth, im.shape[0], im.shape[1], im.shape[2]])
+    target_image = np.zeros([depth, im.shape[0], im.shape[1], im.shape[2]])
+    unc_image = np.zeros([depth, im.shape[0], im.shape[1], im.shape[2]])
 
     for idx in range(depth):
         boxed_image[idx] = imageio.imread(join('/usr/local/data/thomasc/outputs/det_net', str(idx) + '.png'), as_gray=False, pilmode="RGB")
+        nseg_image[idx] = imageio.imread(join('/usr/local/data/thomasc/outputs/det_net', str(idx) + str(idx) + '.png'), as_gray=False, pilmode="RGB")
+        target_image[idx] = imageio.imread(join('/usr/local/data/thomasc/outputs/det_net', 'target' + str(idx) + '.png'), as_gray=False, pilmode="RGB")
+        unc_image[idx] = imageio.imread(join('/usr/local/data/thomasc/outputs/det_net', 'unc' + str(idx) + '.png'), as_gray=False, pilmode="RGB")
     plt.close('all')
     
+    # Images with masks 
     boxed_image = boxed_image.astype(np.uint8)
+    nseg_image = nseg_image.astype(np.uint8)
+    target_image = target_image.astype(np.uint8)
+    unc_image = unc_image.astype(np.uint8)
+    boxed_image = boxed_image.transpose(1, 2, 0, 3)
+    nseg_image = nseg_image.transpose(1, 2, 0, 3)
+    target_image = target_image.transpose(1, 2, 0, 3)
+    unc_image = unc_image.transpose(1, 2, 0, 3)
+    nseg_image = np.rot90(nseg_image, axes=(0,1))
+    boxed_image = np.rot90(boxed_image, axes=(0,1))
+    target_image = np.rot90(target_image, axes=(0,1))
+    unc_image = np.rot90(unc_image, axes=(0,1))
 
+
+    # Base images
     target = target * 255
     target = target.astype(np.uint32).copy()
     threshed = threshed * 255
     threshed = threshed.astype(np.uint32).copy()
-    boxed_image = boxed_image.transpose(1, 2, 0, 3)
-
-    boxed_image = np.rot90(boxed_image, axes=(0,1))
     target = np.rot90(target, axes=(0,1))
     netseg = np.rot90(netseg, axes=(0,1))
     unc = np.rot90(unc, axes=(0,1))
     threshed = np.rot90(threshed, axes=(0,1))
     t2 = np.rot90(t2, axes=(0,1))
 
-    scroll_display(boxed_image, target, threshed, unc, figsize)
+    scroll_display(boxed_image, nseg_image, target_image, unc_image, t2, figsize)
 
